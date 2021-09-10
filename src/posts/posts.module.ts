89ba21a35d776f56a5f62256218a7620e1aeeaaa
avner <avner@zebra-med.com>
2021-09-10 11:20:42 +0300

    auth middleware added

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Post, PostSchema } from './schemas/post.schema';
 import { PostRepository } from './posts.repository';
+import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
 
 @Module({
   imports: [
@@ -12,4 +18,14 @@ import { PostRepository } from './posts.repository';
   controllers: [PostsController],
   providers: [PostRepository, PostsService],
 })
-export class PostsModule {}
+export class PostsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+    consumer
+      .apply(AuthenticationMiddleware)
+      .forRoutes(
+        { method: RequestMethod.POST, path: '/posts' },
+        { method: RequestMethod.PATCH, path: '/posts' },
+        { method: RequestMethod.DELETE, path: '/posts' },
+      );
+  }
+}
